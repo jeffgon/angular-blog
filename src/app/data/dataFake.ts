5330b135a9eade7a9fdb028b7a17efa3cbb58a0e
Jefferson Gonçalves <jeffdevcoder@gmail.com>
2023-10-02 10:56:17 -0300

    feat: content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Homem de Ferro 1 será lançado novamento nos cinemas ",
+    "description": "15 de julho nos cinemas em todo o Brasil",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "2",
+    "title": "Nova série da Marvel será lançada",
+    "description": "blablabla",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
